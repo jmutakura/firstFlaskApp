ecac6b7fe0afc147a2f0c0d0b0615f35e9fbb469
Jonathan Mutakura <jmutakura@gmail.com>
2019-12-09 19:23:08 -0600

    first commit with initial routes

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias animi doloribus expedita facilis maxime minus quaerat quis temporibus. Assumenda                     aut culpa ducimus ea eius incidunt pariatur, qui repellendus sunt!',
+            'author': 'Jay',
+            'create_date': '04-12-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias animi doloribus expedita facilis maxime minus quaerat quis temporibus. Assumenda                     aut culpa ducimus ea eius incidunt pariatur, qui repellendus sunt!',
+            'author': 'Jay',
+            'create_date': '04-12-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias animi doloribus expedita facilis maxime minus quaerat quis temporibus. Assumenda                     aut culpa ducimus ea eius incidunt pariatur, qui repellendus sunt!',
+            'author': 'Jay',
+            'create_date': '04-12-2019'
+        }
+    ]
+
+    return articles
